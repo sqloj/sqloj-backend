2f23bfb6137a7c574158f35e5938494afbc93f82
rogeryoungh <rogeryoungh@foxmail.com>
2022-05-25 20:16:04 +0800

    fix: time-zone

--- src/main/kotlin/pers/sy/sqloj/SqlojBackendApplication.kt
+++ src/main/kotlin/pers/sy/sqloj/SqlojBackendApplication.kt
@@ -2,9 +2,16 @@ package pers.sy.sqloj
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 class SqlojBackendApplication
+
+@PostConstruct
+fun started() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"))
+}
 
 fun main(args: Array<String>) {
     runApplication<SqlojBackendApplication>(*args)
